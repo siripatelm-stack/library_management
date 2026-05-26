8ae943d4a551fa5e8b432ceec92eb89c7b60293c
siripatelm-stack <siripatelm@gmail.com>
2026-05-26 12:39:02 +0530

    Revert "detected an infinite error and resolved"
    This reverts commit 5566791206a3aa38bfa4dbfa072d6fd5eceabb34.

--- student/login.php
+++ student/login.php
@@ -4,11 +4,6 @@ session_start();
 require_once("../config/db.php");
 
 $message = null;
-if (!isset($_SESSION['user'])) {
-    header("Location: ../index.php"); // Or header("Location: /index.php");
-    exit;
-}
-
 
 /* SIGNUP */
 
